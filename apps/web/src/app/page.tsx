5d9dd6f4bc3af668c100c1f486c9c96d3fab089b
jinmin.kim <bmpr14@gmail.com>
2024-09-23 23:55:31 +0900

    feat: add chart component to web/page.tsx

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import {
   Card,
   CardContent,
@@ -5,7 +7,29 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from '@repo/ui/components/card';
+} from "@repo/ui/card";
+import { type ChartConfig, ChartContainer } from "@repo/ui/chart";
+import { Bar, BarChart, CartesianGrid } from "recharts";
+
+const chartData = [
+  { month: "January", desktop: 186, mobile: 80 },
+  { month: "February", desktop: 305, mobile: 200 },
+  { month: "March", desktop: 237, mobile: 120 },
+  { month: "April", desktop: 73, mobile: 190 },
+  { month: "May", desktop: 209, mobile: 130 },
+  { month: "June", desktop: 214, mobile: 140 },
+];
+
+const chartConfig: ChartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "#2563eb",
+  },
+  mobile: {
+    label: "Mobile",
+    color: "#60a5fa",
+  },
+};
 
 function Gradient({
   conic,
@@ -25,28 +49,36 @@ function Gradient({
   );
 }
 
-
 export default function Page(): JSX.Element {
   return (
     <main className="relative min-h-screen p-24 bg-primary">
       <span>hello</span>
-        <Gradient
-          className="absolute top-0 left-0 right-0 bottom-0 opacity-[0.15] w-[800px] h-[800px] m-auto"
-            conic
-          />
-          <Card>
-  <CardHeader>
-    <CardTitle>Card Title</CardTitle>
-    <CardDescription>Card Description</CardDescription>
-  </CardHeader>
-  <CardContent>
-    <p>Card Content</p>
-  </CardContent>
-  <CardFooter>
-    <p>Card Footer</p>
-  </CardFooter>
-</Card>
-
+      <Gradient
+        className="absolute top-0 left-0 right-0 bottom-0 opacity-[0.15] w-[800px] h-[800px] m-auto"
+        conic
+      />
+      <Card>
+        <CardHeader>
+          <CardTitle>Card Title</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p>Card Content</p>
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
+      <ChartContainer
+        className="h</ChartConfig>-[400px] w-full"
+        config={chartConfig}
+      >
+        <BarChart accessibilityLayer data={chartData}>
+          <CartesianGrid vertical={false} />
+          <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
+          <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+        </BarChart>
+      </ChartContainer>
     </main>
   );
 }
